bffaabfaf862f554abd090be2c0f0555622aaea0
asraful196 <asraful.01110@gmail.com>
2021-10-04 21:01:02 +0600

    readme.md added for git

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# SKILL UP 
-* 
-* 
-* git init
-* live Link:  [Create React App](https://github.com/facebook/create-react-app).
+# SKILL UP ONLINE EDUCATIONAL PLATFORM 
+*  SkillUp Online is your ideal learning platform.
+*  Enables you to skill up to the most in-demand technology skills.
+*  SkillUp Online helps you work smarter,get to your career goals faster and create an exciting technology led future
+
+# live Link: [https://skill-up-educational-platform-as-9-by-asraful.netlify.app/](https://skill-up-educational-platform-as-9-by-asraful.netlify.app/).
 
 
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,10 +1,23 @@
 import React from 'react';
-
+import skillUp from '../../image/skillUp.png';
 const Footer = () => {
     return (
-        <div className="" style={{ textAlign: 'center', padding: '3px', backgroundColor: 'DarkSalmon', color: 'white' }}>
-            <p>Author: Hege Refsnes 
-                <a href="mailto:hege@example.com">hege @example.com</a></p>
+        <div className="bg-light " style={{ textAlign: 'center', padding: '3px', backgroundColor: 'DarkSalmon', color: 'gray' }}>
+            <div className="container" style={{ marginBottom: '12px', marginTop: "12px" }}>
+                <img src={skillUp} alt="" />
+                <br />
+                <small>In today’s modern age of disruption, SkillUp Online is your
+                    ideal learning platform that enables you to upskill to the
+                    most in-demand technology skills like Data Science, Big Data,
+                    Artificial Intelligence, Cloud, Front-End Development,
+                    DevOps & many more. In your journey of evolution as a technologist,
+                    SkillUp Online helps you work smarter, get to your career goals faster
+                    and create an exciting technology led future.</small>
+                <br />
+                <div style={{ textDecoration: 'none' }}>
+                    <small > <a href="https://skillup.online/">Copyright ©2021 Skillup. All Rights Reserved</a></small>
+                </div>
+            </div>
         </div>
     );
 };
