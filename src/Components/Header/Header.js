ea15180e682b7674c61d326ea7c1b745c10f81d0
asraful196 <asraful.01110@gmail.com>
2021-10-04 22:32:41 +0600

    readme.md update

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,14 +3,14 @@ import { NavLink } from 'react-router-dom';
 import logo from '../../image/logo.png'
 const Header = () => {
     return (<>
-        <nav className="navbar navbar-expand-lg " style={{ backgroundColor: 'lightGray' }}>
+        <nav className="navbar navbar-expand-lg  navbar-expand-md navbar-expand-sm " style={{ backgroundColor: 'lightGray' }}>
             <div className="container-fluid container ">
                 <img className="img-fluid w-10" src={logo} alt="" />
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <form className="d-flex" style={{ marginLeft: '100px' }}>
+                <div className="collapse navbar-collapse " id="navbarSupportedContent">
+                    <form className="d-flex " style={{ marginLeft: '100px' }}>
                         <input className="form-control me-2 " type="search" placeholder="Search" aria-label="Search" />
                         <button className="btn btn-outline-success" type="submit">Search</button>
                     </form>
